2a401b5fec4b1f5fcd6b4e5487ae69dc5928dfbb
germain loret <germain.loret@gmail.com>
2021-11-10 17:59:27 +0100

    Implement user stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gardens do
+    resources :plants, only: [:create]
+  end
+  resources :plants, only: [:destroy]
 end
